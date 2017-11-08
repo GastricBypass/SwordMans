4a84fec58bc1c8f561e4972fb325e6b90a28f0a3
Drake Poling <dsp45@case.edu>
2017-11-07 21:59:02 -0500

    Several little fixes. Improvements to the highlands stage, wind can now come from either direction and you can fall off and die. Songs are slighly more tailored towards each stage now in an effort to keep the (currently very limited) soundtrack from getting old.

--- Assets/Scripts/Objects/PushingArea.cs
+++ Assets/Scripts/Objects/PushingArea.cs
@@ -6,15 +6,46 @@ public class PushingArea : IEntity {
 
     public Vector3 pushForce;
 
+    public bool changePushForcePeriodically = true;
+    public List<Vector3> possiblePushForces;
+    public float switchTime = 30;
+    public bool useTogglerSwitchTime = true; // requires that a toggler is attached to this gameobject
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Toggler toggler = this.GetComponent<Toggler>();
+        
+        if (toggler != null && useTogglerSwitchTime)
+        {
+            switchTime = (toggler.activeTimeMS + toggler.inactiveTimeMS) / 1000;
+        }
+
+        if (changePushForcePeriodically)
+        {
+            StartCoroutine(ChangeWindForceAfterTime(switchTime));
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public IEnumerator ChangeWindForceAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ChangeWindDirection();
+
+        StartCoroutine(ChangeWindForceAfterTime(time));
+    }
+
+    public void ChangeWindDirection()
+    {
+        int newPushForceIndex = Random.Range(0, possiblePushForces.Count);
+        pushForce = possiblePushForces[newPushForceIndex];
+    }
 
     private void OnTriggerStay(Collider other)
     {
