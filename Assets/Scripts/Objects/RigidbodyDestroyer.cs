0dcf918d51f5b4a82f9846487d5c879f2f80d62e
Drake Poling <dsp45@case.edu>
2018-06-09 15:22:22 -0400

    Progress on Ch 3. Added two new items for summer games

---
+++ Assets/Scripts/Objects/RigidbodyDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyDestroyer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigbod = other.GetComponent<Rigidbody>();
+
+        if (rigbod != null)
+        {
+            Destroy(rigbod.gameObject);
+        }
+    }
+}
