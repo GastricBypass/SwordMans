d38c54fdb4b773f712cf42ccc453df6d32b81042
Drake Poling <dsp45@case.edu>
2019-03-23 20:40:13 -0400

    Added laser sight to some weapons

---
+++ Assets/Scripts/Objects/Laser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float maxLength = 5000;
+    public Vector3 raycastOffset;
+
+    private LineRenderer lineRenderer;
+    private RaycastHit rayHit;
+    private Vector3 lineRenderEndPoint;
+    
+    void Start ()
+    {
+        LineRenderer laser = this.GetComponent<LineRenderer>();
+        if (laser != null)
+        {
+            lineRenderer = laser;
+        }
+        else
+        {
+            lineRenderer = this.gameObject.AddComponent<LineRenderer>();
+        }
+
+        lineRenderEndPoint = lineRenderer.GetPosition(1);
+    }
+	
+	void Update ()
+    {
+        if (Physics.Raycast(transform.position + raycastOffset, -transform.forward, out rayHit))
+        {
+            if (rayHit.collider)
+            {
+                lineRenderer.SetPosition(1, new Vector3(lineRenderEndPoint.x, lineRenderEndPoint.y, -rayHit.distance));
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, new Vector3(lineRenderEndPoint.x, lineRenderEndPoint.y, -maxLength));
+        }
+	}
+}
