57e8cdc3345c175adf7ff70486759e599f112818
GastricBypass <dsp45@case.edu>
2020-03-25 22:35:55 -0400

    Fixed zombie ball targetting multiple players.

---
+++ Assets/Scripts/PlayerControllers/DeferTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeferTarget : IEntity
+{
+    [Tooltip("This AIEnemy will not pick it's own target, it will instead defer to this AIEnemy's target")]
+    public AIEnemy deferTo;
+
+    private AIEnemy attachedAI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attachedAI = gameObject.GetComponent<AIEnemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            attachedAI.target = deferTo.target;
+        }
+    }
+}
